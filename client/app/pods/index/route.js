5be4e0a04f7ea85a59449ec74a9575cad965ce9d
Matt Mazzola <mattmazzola@live.com>
2015-07-29 15:22:16 -0700

    Setup index route with data instead of hardcoded templates. Add scrolling to search categories

--- client/app/pods/index/route.js
+++ client/app/pods/index/route.js
@@ -1,20 +1,84 @@
 import Ember from 'ember';
 
+const categories = [
+  {
+    id: 1,
+    title: 'Incomplete Assignments',
+    items: [
+      {
+        id: 11,
+        title: 'Artificial Intelligence Paper',
+        class: 'CS300',
+        points: 150,
+        dueDate: '2015-07-30T12:00:00Z'
+      },
+      {
+        id: 12,
+        title: 'Assignment 1',
+        class: 'CS300',
+        points: 150,
+        dueDate: '2015-07-30T12:00:00Z'
+      },
+      {
+        id: 13,
+        title: 'Assignment 1',
+        class: 'CS300',
+        points: 150,
+        dueDate: '2015-07-30T12:00:00Z'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Upcoming Tests',
+    items: [
+      {
+        id: 21,
+        title: 'Midterm 1: Static Analysis',
+        class: 'PHYS200',
+        points: 200,
+        dueDate: '2015-07-30T12:00:00Z'
+      },
+      {
+        id: 21,
+        title: 'Midterm 1: Static Analysis',
+        class: 'PHYS200',
+        points: 200,
+        dueDate: '2015-07-30T12:00:00Z'
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Grade Posted',
+    items: [
+      {
+        id: 31
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: 'New Messages',
+    items: [
+      {
+        id: 41
+      },
+      {
+        id: 42
+      },
+      {
+        id: 43
+      },
+      {
+        id: 44
+      }
+    ]
+  }
+];
+
 export default Ember.Route.extend({
   model() {
-    return [
-      {
-        id: 1,
-        name: 'item 1'
-      },
-      {
-        id: 2,
-        name: 'item 2'
-      },
-      {
-        id: 3,
-        name: 'item 3'
-      }
-    ];
+    return categories;
   }
 });
