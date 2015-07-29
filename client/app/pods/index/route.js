73dd67c59de68b1c970c01045672fbc48da18487
Matt Mazzola <mattmazzola@live.com>
2015-07-29 11:41:27 -0700

    Add item specific routes

---
+++ client/app/pods/index/route.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(urlParams) {
+    return [
+      {
+        id: 1,
+        name: 'item 1'
+      },
+      {
+        id: 2,
+        name: 'item 2'
+      },
+      {
+        id: 3,
+        name: 'item 3'
+      }
+    ];
+  }
+});
--- client/app/router.js
+++ client/app/router.js
@@ -6,7 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('timeline');
+  this.route('index', { path: '/' }, function () {
+    this.route('item', { path: '/:item_id' });
+  });
+  this.route('timeline', function() {
+    this.route('event', { path: '/:event_id' });
+  });
 });
 
 export default Router;
