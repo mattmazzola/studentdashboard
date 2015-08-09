176c0ead9d709165d0b8cd606f1517e1f9385109
Matt Mazzola <mattmazzola@live.com>
2015-08-08 22:38:01 -0700

    Update assignment items to have more realistic data

--- client/app/pods/index/route.js
+++ client/app/pods/index/route.js
@@ -16,7 +16,7 @@ const categories = [
         pointsEarned: null,
         pointsTotal: 150,
         description: 'Is Elon Musk really crazy or is AI going to consume the world? Suggested reading: Wait but why? Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        dueDate: '2015-07-30T12:00:00Z',
+        dueDate:  moment().add(2, 'days').toJSON(),
         tags: [
           {
             name: 'homework'
@@ -83,21 +83,87 @@ const categories = [
       {
         id: 12,
         categoryPreviewComponentName: 'sd-search-category-preview-assignment',
-        searchComponentName: 'sa-search-test',
-        componentName: 'sa-test',
-        title: 'Assignment 1',
-        class: 'CS300',
-        points: 150,
-        dueDate: '2015-07-30T12:00:00Z'
+        searchComponentName: 'sa-search-assignment',
+        componentName: 'sa-assignment',
+        title: 'Mad Max: Analysis of post-apocolyptic culture',
+        description: 'Watch Mad Max and try to compare their culture to our current society.  Note the scarcity of resources and conflict. Are there parts of our current world which exhibit these charateristics?',
+        pointsEarned: null,
+        pointsTotal: 80,
+        class: 'PSYC100',
+        dueDate: moment().add(5, 'days').toJSON(),
+        tags: [
+          {
+            name: 'homework'
+          },
+          {
+            name: 'mad max'
+          },
+          {
+            name: 'post-apocolyptic'
+          }
+        ],
+        activities: [
+          {
+            title: 'Read',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            materials: []
+          },
+          {
+            title: 'Complete',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            materials: [
+              {
+                type: 'Word'
+              }
+            ]
+          }
+        ],
+        events: [
+          {
+            date: '2015-07-20T11:05:00Z',
+            title: 'Created'
+          }
+        ]
       },
       {
         id: 13,
-        searchComponentName: 'sa-search-test',
-        componentName: 'sa-test',
-        title: 'Assignment 2',
+        categoryPreviewComponentName: 'sd-search-category-preview-assignment',
+        searchComponentName: 'sa-search-assignment',
+        componentName: 'sa-assignment',
+        title: 'Adamantium Lab: Make the strongest metal on eath',
+        description: 'In this weeks lab we\'re going to have some fun!  By mixing X and Y we will try to produce Adamantium.  Yes, that\'s right you can make the same metal used by Wolverine in X-men.  The group with the highest yeild wins a prize.',
         class: 'CS300',
-        points: 150,
-        dueDate: '2015-07-30T12:00:00Z'
+        pointsEarned: 100,
+        pointsTotal: 150,
+        dueDate: moment().add(7, 'days').toJSON(),
+        tags: [
+          {
+            name: 'lab'
+          },
+          {
+            name: 'adamantium'
+          },
+          {
+            name: 'x-men'
+          }
+        ],
+        activities: [
+          {
+            title: 'Lab',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+            materials: []
+          }
+        ],
+        events: [
+          {
+            date: '2015-07-20T11:05:00Z',
+            title: 'Created'
+          },
+          {
+            date: '2015-07-22T11:05:00Z',
+            title: 'Posted'
+          }
+        ]
       }
     ]
   },
