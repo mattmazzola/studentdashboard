ab3c4f717e3c2ed8334ffc1abdbbceb387cc1d5f
Matt Mazzola <mattmazzola@live.com>
2015-08-09 18:10:49 -0700

    Change timeline to use events from route, allow event circles to be clicked to show specific event.

--- app/pods/components/sa-timeline/component.js
+++ app/pods/components/sa-timeline/component.js
@@ -10,11 +10,13 @@ export default Ember.Component.extend({
   classNames: ['sa-timeline__inner'],
 
   setupSvg: on('didInsertElement', function () {
+
+    var $ = window.jQuery;
     const height = 800;
   	const width = 2000;
-  	const aspect = Math.floor(height / width);
-  	const paddingLeft = 100;
-  	const paddingBottom = 100;
+  	//const aspect = Math.floor(height / width);
+  	//const paddingLeft = 100;
+  	const paddingBottom = 200;
   	const classLegendX = 130;
   	const rootSelector = ".sa-timeline__inner";
   	const root = $(rootSelector);
@@ -31,39 +33,11 @@ export default Ember.Component.extend({
   			preserveAspectRatio: 'xMidYMid'
   		});
 
-  	const assignments = [
-  		{
-  			dueDate: '2015-07-28T00:00:00Z',
-  			title: 'Assignment 1',
-  			description: '',
-  			classId: 1,
-  		},
-  		{
-  			dueDate: '2015-07-30T00:00:00Z',
-  			title: 'Assignment 2',
-  			description: '',
-  			classId: 2,
-  		},
-  		{
-  			dueDate: '2015-08-05T00:00:00Z',
-  			title: 'Assignment 3',
-  			description: '',
-  			classId: 1,
-  		},
-  		{
-  			dueDate: '2015-08-10T00:00:00Z',
-  			title: 'Assignment 4',
-  			description: '',
-  			classId: 2,
-  		}
-  	];
-
-
-
+  	const assignments = this.get('events');
 
   	// create x-scale
-  	const minDomainDate = moment('2015-07-25T00:00:00Z');
-  	const maxDomainDate = moment('2015-08-10T00:00:00Z');
+  	const minDomainDate = moment('2015-08-01T00:00:00Z');
+  	const maxDomainDate = moment('2015-08-30T00:00:00Z');
   	const currentDateMoment = moment().set({
   		hours: 0,
   		minutes: 0,
@@ -76,7 +50,8 @@ export default Ember.Component.extend({
   		.range([classLegendX, width-classLegendX])
   	;
 
-  	const currentDateLine = svg
+    // Add current date line.
+  	svg
   		.append("line")
   		.attr({
   			x1: xScale(currentDateMoment),
@@ -88,12 +63,11 @@ export default Ember.Component.extend({
   		})
     ;
 
-
   	// Add x axises
   	const xAxisTicks = d3.svg.axis()
   		.scale(xScale)
   		.orient("bottom")
-  		.ticks(d3.time.days, 2)
+  		.ticks(d3.time.days, 1)
   	;
 
   	const xAxisOffsets = d3.svg.axis()
@@ -112,7 +86,7 @@ export default Ember.Component.extend({
   		.scale(xScale)
   		.orient("bottom")
   		.ticks(d3.time.days, 2)
-  		.tickFormat(date => moment(date).format('M/d'))
+  		.tickFormat(date => moment(date).format('M/D'))
   	;
 
   	const xAxisDays = d3.svg
@@ -120,7 +94,7 @@ export default Ember.Component.extend({
   		.scale(xScale)
   		.orient("bottom")
   		.ticks(d3.time.days, 2)
-  		.tickFormat(date => moment(date).format('dd'))
+  		.tickFormat(date => moment(date).format('ddd'))
   	;
 
   	// Add X-axis ticks
@@ -205,23 +179,28 @@ export default Ember.Component.extend({
 
   	const scaledDate = event => xScale(stripNonDate(moment(event.dueDate)));
 
-  	const circles = svg
+  	//const circles =
+    svg
   		.selectAll("xyz")
   		.data(assignments)
   		.enter()
   		.append("circle")
   		.attr({
   			cx: scaledDate,
-  			cy: d => height - (100 + 30 + ((d.classId === 1) ? 5  : 35)),
+  			cy: d => height - (paddingBottom + 30 + ((d.classId === 1) ? 5  : 35)),
   			r: 10
   		})
   		.style({
   			fill: '#965b93'
   		})
   		.text(d => d.title)
+      .on('click', (d,i) => {
+        this.sendAction('action', d);
+      })
   	;
 
-  	const lines = svg
+  	//const lines =
+    svg
   		.selectAll("xyz")
   		.data(assignments)
   		.enter()
@@ -230,7 +209,7 @@ export default Ember.Component.extend({
   			x1: scaledDate,
   			y1: height - paddingBottom,
   			x2: scaledDate,
-  			y2: d => height - (100 + 30 + ((d.classId === 1) ? 5  : 35)),
+  			y2: d => height - (paddingBottom + 30 + ((d.classId === 1) ? 5  : 35)),
   			'stroke-width': 2,
   			'stroke': '#965b93'
   		})
--- app/pods/timeline/route.js
+++ app/pods/timeline/route.js
@@ -3,18 +3,42 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return [
-      {
-        id: 1,
-        title: 'item 1'
-      },
-      {
-        id: 2,
-        title: 'item 2'
-      },
-      {
-        id: 3,
-        title: 'item 3'
-      }
-    ];
+  		{
+  			dueDate: '2015-08-03T08:00:00Z',
+  			title: 'Assignment 1',
+  			description: 'zxcvzxcv',
+  			classId: 1,
+  		},
+  		{
+  			dueDate: '2015-08-10T08:00:00Z',
+  			title: 'Assignment 2',
+  			description: 'asdfasd',
+  			classId: 2,
+  		},
+  		{
+  			dueDate: '2015-08-16T08:00:00Z',
+  			title: 'Assignment 3',
+  			description: 'wqerqwe',
+  			classId: 1,
+  		},
+  		{
+  			dueDate: '2015-08-17T08:00:00Z',
+  			title: 'Assignment 4',
+  			description: 'qwerqw',
+  			classId: 2,
+  		},
+  		{
+  			dueDate: '2015-08-18T08:00:00Z',
+  			title: 'Assignment 5',
+  			description: '23452345262345',
+  			classId: 2,
+  		}
+  	];
+  },
+
+  actions: {
+    itemClicked(item) {
+      this.transitionTo("timeline.event", item)
+    }
   }
 });
