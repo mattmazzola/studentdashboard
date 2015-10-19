122cc18e59febed5460f697c5d4aa5f80bfe3c16
Matt Mazzola <mattmazzola@live.com>
2015-10-18 20:44:01 -0700

    Add more styling to tab controls.

--- app/pods/components/sd-timeline-controls/component.js
+++ app/pods/components/sd-timeline-controls/component.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isClassesMenuVisible: false,
+
+  actions: {
+    classesCommandClicked() {
+      console.log(`commandClicked`);
+      this.toggleProperty('isClassesMenuVisible');
+    },
+
+    dropDownItemClicked() {
+      console.log(`dropDownItemClicked`);
+    },
+
+    projectionsCommandClicked() {
+      console.log(`commandClicked`);
+    },
+
+    holidaysCommandClicked() {
+      console.log(`commandClicked`);
+    }
+  }
 });
