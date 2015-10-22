8a946066401af76c4dc3f0acaa1b31eee8b81008
Matt Mazzola <mattmazzola@live.com>
2015-10-22 00:13:40 -0700

    Add active tab and active commands behavior

--- app/pods/components/sd-timeline-controls/component.js
+++ app/pods/components/sd-timeline-controls/component.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
+const {
+  computed,
+  on
+} = Ember;
+
 export default Ember.Component.extend({
+  activeTab: null,
   isClassesMenuVisible: false,
+
+  activeCommands: computed('activeTab', function() {
+    return this.get('activeTab.commands');
+  }),
 
   actions: {
     classesCommandClicked() {
@@ -13,12 +23,81 @@ export default Ember.Component.extend({
       console.log(`dropDownItemClicked`);
     },
 
+    holidaysCommandClicked() {
+      console.log(`commandClicked`);
+    },
+
     projectionsCommandClicked() {
       console.log(`commandClicked`);
     },
 
-    holidaysCommandClicked() {
-      console.log(`commandClicked`);
+    selectTab(tab) {
+      this.set('activeTab', tab);
     }
-  }
+  },
+
+  configuration: {
+    tabs: [
+      {
+        name: 'View',
+        commands: [
+          {
+            name: 'Classes',
+            iconClass: 'ms-Icon--multiChoice',
+            dropdown: true,
+            dropdownItems: [
+              {
+                name: 'All',
+                iconClass: 'ms-Icon--star',
+                divider: true
+              },
+              {
+                iconClass: 'ms-Icon--star',
+                name: 'PHYS200'
+              },
+              {
+                iconClass: 'ms-Icon--star',
+                name: 'PSYC100'
+              },
+              {
+                iconClass: 'ms-Icon--star',
+                name: 'CHEM230'
+              },
+              {
+                iconClass: 'ms-Icon--star',
+                name: 'MATH100'
+              }
+            ]
+          },
+          {
+            name: 'Projections',
+            iconClass: 'ms-Icon--checkbox',
+            dropdown: false
+          },
+          {
+            name: 'Holidays',
+            iconClass: 'ms-Icon--checkbox',
+            dropdown: false
+          }
+        ]
+      },
+      {
+        name: 'Insert',
+        commands: [
+          {
+            name: 'Assignment',
+            iconClass: 'ms-Icon--checkbox',
+            dropdown: false
+          }
+        ]
+      }
+    ]
+  },
+
+  onInit: on('init', function () {
+    console.log(`init()`);
+    const tab = this.get('configuration').tabs[0];
+    this.set('activeTab', tab);
+  })
+
 });
